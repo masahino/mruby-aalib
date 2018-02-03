def225f78e6628ce3e617eab0fff07526801365c
masahino <masahino@gmail.com>
2018-02-03 22:04:13 +0900

    fix

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,7 +1,6 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
-  conf.gem File.expand_path(File.dirname(__FILE__))+"/mruby/mrbgems/mruby-io"
   conf.gem File.expand_path(File.dirname(__FILE__)) do |g|
     g.download_aalib
   end
